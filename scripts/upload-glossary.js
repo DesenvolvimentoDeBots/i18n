4b6701569ac5725dac3f507a21f9d09cb1bf76d5
TiagoDanin <TiagoDanin@outlook.com>
2018-06-05 13:09:18 -0300

    Add script upload-glossary

---
+++ scripts/upload-glossary.js
@@ -0,0 +1,30 @@
+const crowdinGlossary = require('crowdin-glossary')
+const jsonfile = require('jsonfile')
+const glossary = crowdinGlossary({
+	project: 'i18n',
+	crowdinKey: '',
+	//languageCode: 'ptbr' //https://github.com/crowdin-modules/crowdin-glossary/pull/8
+})
+
+var data = jsonfile.readFileSync('api.json')
+const main = async () => {
+	var typeTg = []
+	const addTypeTg = (obj, type) => {
+		obj[type].forEach(e => {
+			if (e.fields) {
+				e.fields.forEach(field => {
+					if (!typeTg.includes(field.field)) {
+						typeTg.push(field.field)
+					} else if (!typeTg.includes(field.type)) {
+						typeTg.push(field.type)
+					}
+				})
+			}
+		})
+	}
+	addTypeTg(data, 'Types')
+	addTypeTg(data, 'Methods')
+	typeTg.forEach(term => glossary.add(term, 'Isso talvez não seja traduzivel'))
+	glossary.upload()
+}
+main()
